4d557e19c2cdfa54b316cd4575b28775abb2fc65
Ronald Mundell <ronniemundell8@gmail.com>
2021-05-27 22:47:25 -0700

    Made Add/Delete buttons

--- src/main/resources/public/calculator.js
+++ src/main/resources/public/calculator.js
@@ -1,4 +1,5 @@
-var CalcArr = new Array();
+var CalcArr = new Array(4);
+var Rows = 4;
 function CalculatePercent(number){
     var top = document.getElementById("top"+number).value;
     var bottom = document.getElementById("bottom"+number).value;
@@ -10,14 +11,16 @@ function CalculatePercent(number){
     Percent = Math.round(Percent*100)+"%"
     CalcArr[(number-1)] = Percent
     }
+    document.getElementById("top"+number).value = top;
+    document.getElementById("bottom"+number).value = bottom;
     document.getElementById("precent"+number).textContent = Percent
 }
 function CalculateMean(){
     var Result = ""
     var FinalNum = 0;
     var count = 0;
-    for(var i = 0; i < 4; i++){
-        if(CalcArr[i] == undefined||CalcArr[i] == "N/A"){
+    for(var i = 0; i < Rows; i++){
+        if(CalcArr[i] == undefined||CalcArr[i] == "N/A"||CalcArr[i] == ""){
         }else{
             count++;
             Result += "A"+(i+1)+" "+CalcArr[i]+", "
@@ -27,14 +30,19 @@ function CalculateMean(){
     
     Result = Result.slice(0, Result.length-2)+":"
     var Result2 = "Mean: "+Math.round(FinalNum/count)+"%"
+    if(Result == ":"){
+        document.getElementById("result1").textContent = "Please insert grade values";
+        document.getElementById("result2").textContent = "";
+    }else{
     document.getElementById("result1").textContent = Result;
     document.getElementById("result2").textContent = Result2;
+    }
 }
 function CalculateWeight(){
     var Result = ""
     var FinalNum = 0;
     var count = 0;
-    for(var i = 0; i < 4; i++){
+    for(var i = 0; i < Rows; i++){
         var weight = document.getElementById("weight"+(i+1)).value
         if(CalcArr[i] == undefined||CalcArr[i] == "N/A"|| weight == ""){
         }else{
@@ -46,6 +54,30 @@ function CalculateWeight(){
     
     Result = Result.slice(0, Result.length-2)+":"
     var Result2 = "Weight: "+Math.round(FinalNum/count)+"%"
+    if(Result == ":"){
+        document.getElementById("result1").textContent = "Please insert weight and grade values";
+        document.getElementById("result2").textContent = "";
+    }else{
     document.getElementById("result1").textContent = Result;
     document.getElementById("result2").textContent = Result2;
+    }
+}
+
+function newRow(){
+    CalcArr.push("")
+    Rows++;
+    var tablerow = document.getElementById("T"+(Rows-1));
+   tablerow.insertAdjacentHTML("afterend","<tr id=T"+Rows+"><td>Activity "+Rows+"</td><td>A"+Rows+"</td><td>"+
+   '<input id="weight'+Rows+'" class="TextBox" type="number"/></td>'+
+   '<td><input id="top'+Rows+'" class="TextBox" type="number" onchange="CalculatePercent('+Rows+')"/> <b>/</b>'+
+   '<input id="bottom'+Rows+'" class="TextBox" type="number" onchange="CalculatePercent('+Rows+')"/></td>'+
+  '<td><p id="precent'+Rows+'"></p></td></tr>')
+}
+function deleteRow(){
+    if(Rows == 1){}else{
+    var tablerow = document.getElementById("T"+Rows);
+    CalcArr.pop();
+    Rows--;
+    tablerow.remove()
+    }
 }
