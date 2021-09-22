8666d957564e36514caefa6c7aa062ebfb7511d8
Ronald Mundell <ronniemundell8@gmail.com>
2021-09-21 21:06:58 -0700

    Done

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -61,7 +61,6 @@ public class Main {
 
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
-      stmt.executeUpdate("DROP TABLE images");
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS images (id serial, alttext varchar(50), imgname varchar(50), imgurl TEXT)");
       String sql = "SELECT * FROM images";
       ResultSet rs = stmt.executeQuery(sql);
